b0dc6b5d3e5d5c19d4d651c0f884c0dd4f9d36c0
Grant Ward <ggrantward@gmail.com>
2018-05-30 22:07:44 -0400

    Add kivy files

---
+++ gui/_kivy.py
@@ -0,0 +1,9 @@
+"""
+Module providing a Kivy GUI for game.
+https://kivy.org
+"""
+import kivy
+
+
+if __name__ == "__main__":
+    pass
---
+++ gui/test_kivy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Test Kivy GUI.
+"""
+import _kivy
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
