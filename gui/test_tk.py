2d4a6f2fda2c808a88258437881ce8862e4af5f0
Grant Ward <ggrantward@gmail.com>
2018-05-30 21:42:14 -0400

    Add Game_Window class and test

---
+++ gui/test_tk.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module evaluates the tk element of the
+easy_game/gui package.
+"""
+import tk
+
+
+def main():
+    window = tk.Game_Window()
+
+
+if __name__ == "__main__":
+    main()
--- gui/tk.py
+++ gui/tk.py
@@ -1,2 +1,22 @@
+"""
+Module providing a basic Tkinter GUI for a game.
+"""
+import tkinter
+
+
+class Game_Window(tkinter.Tk):
+    """
+    Extends tkinter.Tk.
+    Provides a basic game GUI window.
+    """
+    def __init__(self):
+        """
+        Generate a new Game Window.
+        """
+        # Initialize tk
+        super().__init__()
+
+
+
 if __name__ == "__main__":
     pass
